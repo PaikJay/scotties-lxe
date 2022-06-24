2d45593a2927a8182b02b64561b9c4f1d314d817
Paik Jay <51130958+PaikJay@users.noreply.github.com>
2022-06-24 16:58:03 -0400

    change about me

--- src/App.js
+++ src/App.js
@@ -144,10 +144,28 @@ export default class App extends React.Component {
         </div>
       
     </section>
+     <section id="testimonials" className="tm-section-pad-top tm-parallax-2">      
+    <div className="container tm-testimonials-content">
+      <div className="row">
+        <div className="col-lg-12 tm-content-box">
+          <h2 className="text-white text-center mb-4 tm-section-title">About Me</h2>
+          <div className="mx-auto tm-gallery-container tm-gallery-container-2">
+            <div className="tm-testimonials-carousel">
+              <figure className="tm-testimonial-item tm-image">
+                <img src="img/testimonial-img-02.jpeg" alt="testimonial" className="img-fluid mx-auto " />
+                <blockquote></blockquote>
+                <figcaption></figcaption>
+              </figure>
+
+            
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div className="tm-bg-overlay"></div>
+  </section>
     
-    <section id="testimonials" className="tm-section-pad-top tm-parallax-2">      
-    <AboutPage />
-    </section>
     
     <section id="gallery" className="tm-section-pad-top">
       <div className="container tm-container-gallery">
