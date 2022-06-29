3654d696e2cf103fa3a46b0d48956e6b5b0aa57b
Paik Jay <51130958+PaikJay@users.noreply.github.com>
2022-06-29 17:35:47 -0400

    change hashRouter name url

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM, { createRoot } from 'react-dom/client' ;
+import { HashRouter } from 'react-router-dom'
 import './index.css';
 import App from './App';
 import "bootstrap/dist/css/bootstrap.min.css";
 
 
 const root = createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+    <HashRouter>
+<App />
+    </HashRouter>
+);
